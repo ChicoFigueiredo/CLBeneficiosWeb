d89162b2494638d89e304810d136b86066b9cd05
Francisco Lima Figueiredo <fran.fig@gmail.com>
2017-10-09 23:02:01 -0300

    Atualizado

--- CLBeneficiosWeb/Models/CLBeneficios.Designer.cs
+++ CLBeneficiosWeb/Models/CLBeneficios.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\franf\onedrive\documentos\visual studio 2017\Projects\CLBeneficiosWeb\CLBeneficiosWeb\Models\CLBeneficios.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\franf\OneDrive\Documentos\Visual Studio 2017\Projects\CLBeneficiosWeb\CLBeneficiosWeb\Models\CLBeneficios.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
